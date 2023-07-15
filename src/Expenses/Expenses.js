81699427eb0f109dce53f09e90a722147e06af39
smart <mail@mail.com>
2023-07-15 19:44:10 +0200

    challenge solved

--- src/Expenses/Expenses.js
+++ src/Expenses/Expenses.js
@@ -1,11 +1,25 @@
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
+import {useState} from "react";
+
 
 // default export function Expenses(props){
 const Expenses = (props) => {
+
+    const [selectedYearFilter, setselectedYearFilter] = useState('2022');
+
+    const selectedYearFilterHandler = selectedYear => {
+        setselectedYearFilter(selectedYear);
+        console.log('Expenses: \n' + selectedYear + '\n' +props);
+    };
+
     return(
+        <div>
         <Card className="expenses">
+            <ExpensesFilter selected={selectedYearFilter} onChangeYearFilterOnExpenses={selectedYearFilterHandler}/>
+
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -27,6 +41,7 @@ const Expenses = (props) => {
                 date={props.items[3].date}
             />
         </Card>
+        </div>
     )
 }
 
--- src/ExpensesFilter/ExpensesFilter.js
+++ src/ExpensesFilter/ExpensesFilter.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpensesFilter.css';
 
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+
+    const selectionHandler = (event) => {
+        props.onChangeYearFilterOnExpenses(event.target.value);
+    };
+
     return (
         <div className="expenses-filter">
             <div className="expenses-filter__control">
                 <label> Filter by Year </label>
-                <select>
-                    <option value="2022"> 2022</option>
-                    <option value="2022"> 2021</option>
-                    <option value="2022"> 2020</option>
+                <select value={props.selected} onChange={selectionHandler}>
+                    <option value="2022" > 2022</option>
+                    <option value="2021"> 2021</option>
+                    <option value="2020"> 2020</option>
                     <option value="2019"> 2019</option>
                 </select>
             </div>
