f7e2979add3af2f9f254dae7cc98999666d8822a
smart <mail@mail.com>
2023-07-17 17:26:16 +0200

    Share for bug fix

--- src/Expenses/Expenses.js
+++ src/Expenses/Expenses.js
@@ -15,20 +15,34 @@ const Expenses = (props) => {
         console.log('Expenses: \n' + selectedYear + '\n' +props);
     };
 
+    const filteredExpensesByYear = props.items.filter(expenese => {
+        return expenese.date.getFullYear().toString() === selectedYearFilter;
+    })
+
+    let expensesContent = <p> No Expenses found in that year</p>;
+
+    if (filteredExpensesByYear.length > 0) {
+        expensesContent =   filteredExpensesByYear.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            )
+        )
+    }
+
     return(
         <div>
         <Card className="expenses">
+
             <ExpensesFilter
                 selected={selectedYearFilter}
                 onChangeYearFilterOnExpenses={selectedYearFilterHandler}
             />
-            {props.items.map((expense) => (
-                <ExpenseItem
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                    />
-            ))}
+
+            {expensesContent}
 
         </Card>
         </div>
