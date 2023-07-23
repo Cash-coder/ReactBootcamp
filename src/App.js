1ee2a5cd13cccd48eedc0daa06d5c6e6e07e9d87
smart <mail@mail.com>
2023-07-23 21:06:01 +0200

    1º commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,33 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
-import componentsImage from './assets/images/components.png';
-import stateImage from './assets/images/state.png';
-import eventsImage from './assets/images/events.png';
-import Header from "./components/components/Header";
-import Concept from "./components/components/Concept";
+import React from 'react';
 
-const concepts = [
-  {
-    title: 'Components',
-    image: componentsImage,
-    description:
-      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
-  },
-  {
-    title: 'State',
-    image: stateImage,
-    description:
-      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
-  },
-  {
-    title: 'Events',
-    image: eventsImage,
-    description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
-  },
-];
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <ul id="Concepts">
-        <Concept
-            image={concepts[0].image}
-            title={concepts[0].title}
-            description={concepts[0].description}
-        />
-        <Concept
-            image={concepts[1].image}
-            title={concepts[1].title}
-            description={concepts[1].description}
-        />
-        <Concept
-            image={concepts[2].image}
-            title={concepts[2].title}
-            description={concepts[2].description}
-        />
-      </ul>
-    </div>
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
+  return (
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
